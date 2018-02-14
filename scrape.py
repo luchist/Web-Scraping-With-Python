05d31fc2eb032003b4289fe0e59a9559234cf5bd
Luis Coronel <lucho_9292@live.com>
2018-02-13 23:57:16 -0300

    fixed

--- scrape.py
+++ scrape.py
@@ -1,14 +1,14 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-#HTTP error handling
+# HTTP error handling
 try:
- html = urlopen("http://www.pythonscraping.com/pages/page1.html")
+    html = urlopen("http://www.pythonscraping.com/pages/page8984.html")
 except HTTPError as e:
- print(e)
- #return null, break, or do some other "Plan B"
+    print(e)
+# return null, break, or do some other "Plan B"
 else:
- #program continues. Note: If you return or break in the
- #exception catch, you do not need to use the "else" statement
- bsObj = BeautifulSoup(html.read(),"html.parser")
- print(bsObj.div)
+    # program continues. Note: If you return or break in the
+    # exception catch, you do not need to use the "else" statement
+    bsObj = BeautifulSoup(html.read(), "html.parser")
+    print(bsObj.div)
